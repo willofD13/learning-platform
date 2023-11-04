7e70411830ad3672d7841b83c82631dbc518a616
willofD13 <leonidastsiakas13@gmail.com>
2023-11-04 15:39:02 +0200

    Fix typos in course model validations

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,4 @@
 class Course < ApplicationRecord
-    validates :title, presence:true, length: {maximun: 10}, uniqueness: true
-    validates :description, presence: true, length: {maximun: 100}
+    validates :title, presence:true, length: {maximum: 10}, uniqueness: true
+    validates :description, presence: true, length: {maximum: 100}
 end
