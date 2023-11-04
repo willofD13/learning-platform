cbb03f9b68243de8912571b7b4060141b38c4d78
willofD13 <leonidastsiakas13@gmail.com>
2023-11-04 15:42:56 +0200

    Create course model

---
+++ db/migrate/20231104134231_create_lessons.rb
@@ -0,0 +1,11 @@
+class CreateLessons < ActiveRecord::Migration[7.1]
+  def change
+    create_table :lessons do |t|
+      t.string :title
+      t.text :body
+      t.references :course, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
