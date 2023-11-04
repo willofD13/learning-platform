cfc29a484d1eae56ddcad61542b60390dba8c1b2
willofD13 <leonidastsiakas13@gmail.com>
2023-11-04 15:30:55 +0200

    Add validations to course

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,4 @@
 class Course < ApplicationRecord
+    validates :title, presence:true, length: {maximun: 10}, uniqueness: true
+    validates :description, presence: true, length: {maximun: 100}
 end
