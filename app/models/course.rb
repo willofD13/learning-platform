738a6c0a54ef92796da47d7cf9c4534e9afc5ec2
willofD13 <leonidastsiakas13@gmail.com>
2023-11-04 15:47:44 +0200

    Write a has_many association in course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,4 +1,6 @@
 class Course < ApplicationRecord
+    has_many :lessons
+    
     validates :title, presence:true, length: {maximum: 10}, uniqueness: true
     validates :description, presence: true, length: {maximum: 100}
 end
